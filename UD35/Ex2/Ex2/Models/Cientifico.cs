afd1675be10efc97cf7048981bd5c750211c257a
Jorgyg <jgraslorenzo@gmail.com>
2023-09-12 17:22:32 +0200

    API

--- UD35/Ex2/Ex2/Models/Cientifico.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Ex2.Models
-{
-    public partial class Cientifico
-    {
-        public string Dni { get; set; } = null!;
-        public string? NomApels { get; set; }
-        public virtual ICollection<Proyecto> Proyectos { get; set; } = new List<Proyecto>();
-    }
-}
